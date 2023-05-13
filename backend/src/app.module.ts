5fa1d184c45f01f51fc47c3dded36a88ca82c435
HungNg1012 <hung.nguyen1012@hcmut.edu.vn>
2023-05-13 09:31:33 +0700

    Done CRUD method for User and Station

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose'; 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StationModule } from './station/station.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
+  imports: [MongooseModule.forRoot('mongodb+srv://busdy:busdy123@busdy.kchewci.mongodb.net/?retryWrites=true&w=majority'), StationModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Post,
+    Put,
+  } from '@nestjs/common';
+  import { CreateUserDto } from './dto/create-user.dto';
+  import { UpdateUserDto } from './dto/update-user.dto';
+  import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private readonly service: UserService) {}
+
+  @Get()
+  async index() {
+    return await this.service.findAll();
+  }
+
+  @Get(':id')
+  async find(@Param('id') id: string) {
+    return await this.service.findOne(id);
+  }
+
+  @Post()
+  async create(@Body() createUserDto: CreateUserDto) {
+    return await this.service.create(createUserDto);
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return await this.service.update(id, updateUserDto);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    return await this.service.delete(id);
+  }
+}
